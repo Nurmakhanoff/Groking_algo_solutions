7321c56868f983a2e0e7e80b8a14a0f46c61e0eb
Kuat <kuatnurmakhanov@gmail.com>
2023-07-09 20:34:00 +0600

    Create Ex-4.6.js

--- July/09.07.23/Ex-4.6.js
+++ July/09.07.23/Ex-4.6.js
@@ -1,8 +1,9 @@
 //How long would each of these operations take in Big O notation?
-//4.5 Printing the value of each element in an array.
+//4.6 Doubling the value of each element in an array.
 const arr = [2, 4, 6, 8, 10];
 
 for (let i = 0; i < arr.length; i++) {
-  console.log(arr[i]);
+    arr[i] = arr[i]*2;
+    console.log(arr[i])
 }
 // Answer is O(n). This is because the algorithm must work through each element of the array and it will take time proportional to the size of the array.
